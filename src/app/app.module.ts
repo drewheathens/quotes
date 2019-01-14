7cbb8fcb8125e2d1fc626405aaf1ca34bf008bef
drewheathens <mudavadie@gmail.com>
2019-01-14 08:23:48 +0300

    added button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteFormComponent,
+    ReactiveFormsComponent
   ],
   imports: [
     BrowserModule
