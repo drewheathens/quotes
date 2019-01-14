7971dd997fd53d7541d8978d0025762b0ad86192
drewheathens <mudavadie@gmail.com>
2019-01-14 09:47:21 +0300

    added form and button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
-import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+// import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteFormComponent,
-    ReactiveFormsComponent
+    // ReactiveFormsComponent
   ],
   imports: [
     BrowserModule
